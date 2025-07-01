79c52783a5840b7e6ad720ae12292573447de796
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-07-01 20:03:04 +0200

    RolePermissionSeeder

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -5,14 +5,18 @@ namespace Database\Seeders;
 // database/seeders/RolePermissionSeeder.php
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class RolePermissionSeeder extends Seeder
 {
     public function run()
     {
-        // Crear permisos
+        // Limpiar caché de permisos
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Crear permisos si no existen
         $permissions = [
             'manage-users',
             'manage-books',
@@ -20,23 +24,21 @@ class RolePermissionSeeder extends Seeder
             'manage-own-loans',
             'view-reports'
         ];
-
         foreach ($permissions as $permission) {
-            Permission::create(['name' => $permission, 'guard_name' => 'web']);
+            Permission::firstOrCreate(['name' => $permission, 'guard_name' => 'web']);
         }
 
-        // Crear roles
-        $adminRole = Role::create(['name' => 'admin']);
-        $userRole = Role::create(['name' => 'user']);
+        // Crear roles si no existen
+        $adminRole = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        $userRole  = Role::firstOrCreate(['name' => 'user',  'guard_name' => 'web']);
 
         // Asignar permisos a roles
         $adminRole->givePermissionTo([
             'manage-users',
-            'manage-books', 
+            'manage-books',
             'manage-all-loans',
             'view-reports'
         ]);
-
         $userRole->givePermissionTo(['manage-own-loans']);
     }
 }
