935d34aa8d7077fecfe2a053ce87fc4d1a1b0435
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-07-02 17:52:43 +0200

    Modelo usuario con autenticación

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,12 +4,11 @@ namespace App\Models;
 
 // app/Models/Usuario.php
 
-
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Sanctum\HasApiTokens;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
     use HasRoles, HasApiTokens;
     protected $guard_name = 'web';
